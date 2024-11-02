67913947d43dda2a1f66a215fc231b02d1900ddd
inushapeiris <inushaumayanthapeiris@gmail.com>
2024-11-02 16:43:34 +0530

    file structure change

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
-import Content from './components/Content.vue';
+import Content from './components/Topic-on-the-left-and-the-content-on-right/Content.vue';
+import Accordian from './components/Accordian.vue';
 </script>
 
 <template>
   <div>
-    <Content />
+    <!-- <Content /> -->
+     <Accordian />
   </div>
 </template>
