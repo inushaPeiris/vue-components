ca05ea20124cd93abf6e6591b638ecb7db6fe669
inushapeiris <inushaumayanthapeiris@gmail.com>
2024-11-14 16:28:05 +0530

    Adding component to display the hmlt with quill styles

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@ import Content from "./components/Topic-on-the-left-and-the-content-on-right/Con
 import Accordian from "./components/Accordian/Accordian.vue";
 import Courses from "./components/slidable-title-bar/Courses.vue";
 import DataProvider from "./components/slidable-title-bar/DataProvider.vue";
-import RichTextEditor from "./components/VueQuill/RichTextEditor.vue";
+import RichTextEditor from "./components/VueQuill-rich-text-editor/RichTextEditor.vue";
+import DisplayHtmlContent from "./components/VueQuill-rich-text-editor/DisplayHtmlContent.vue";
 </script>
 
 <template>
   <div>
     <h1>Vue 3 Quill Editor</h1>
     <RichTextEditor />
+    <DisplayHtmlContent />
   </div>
 </template>
---
+++ src/components/VueQuill-rich-text-editor/DisplayHtmlContent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="content-container">
+    <!-- Display component -->
+    <div class="ql-editor">
+      <h2 class="mb-4">Rendered Content</h2>
+      <div 
+        class="ql-content-display"
+        v-html="htmlContent"
+      ></div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import 'quill/dist/quill.core.css'
+
+const htmlContent = ref('<p class="ql-align-center"><span class="ql-size-huge">Announcement</span></p><p class="ql-align-center"><br></p><ul><li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li><li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li><li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li><li>Lorem ipsum dolor sit amet, consectetur adipiscing elitfdfd</li></ul><p class="ql-align-center"><br></p><p class="ql-align-center"><br></p>');
+</script>
+
+<style scoped>
+.content-container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
+</style>
+
